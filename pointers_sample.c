c8b380b061cc5322946ff993fa52cecd82a5e143
spike <weisuojiaoshou@gmail.com>
2015-12-01 11:44:57 +0900

    more about pointers

--- pointers_sample.c
+++ pointers_sample.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int count = 10, x;
-    int *int_pointer;
-    
-    int_pointer = &count;
-    x = *int_pointer;
-    
-    printf("count = %i, x = %i\n" , count , x);
-    
+ 		char c = 'Q';
+ 		char *char_pointer = &c;
+
+ 		printf ("%c %c\n", c, *char_pointer);
+
+    c = '/';
+    printf ("%c %c\n", c, *char_pointer);
+
+    *char_pointer = '(';
+    printf ("%c %c\n", c, *char_pointer);
+
     return 0;
 }
