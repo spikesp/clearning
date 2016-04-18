f91065cd0e84ebfca60abb59ae78ec152107ffdc
spike <weisuojiaoshou@gmail.com>
2016-04-18 11:18:27 +0900

    add a new sample

---
+++ string_combination/string_combination/main.c
@@ -0,0 +1,43 @@
+//
+//  main.c
+//  string_combination
+//
+//  Created by spike on 16/4/18.
+//  Copyright © 2016年 ___spike___. All rights reserved.
+//
+
+#include <stdio.h>
+
+void concat (char result[], const char str1[], int n1, const char str2[], int n2)
+{
+    int i, j;
+    
+    //copy str1 to result
+    
+    for ( i = 0; i < n1; ++i )
+        result[i] = str1[i];
+    
+    //copy str2 to result
+    for ( j = 0; j < n2; ++j)
+        result[n1 + j] = str2[j];
+}
+
+int main(int argc, const char * argv[]) {
+    
+    void concat (char result[], const char str1[], int n1, const char str2[], int n2);
+    const char s1[5] = { 'T', 'e', 's', 't', ' '};
+    const char s2[6] = { 'w', 'o', 'r', 'k', 's', '.'};
+    char s3[11];
+    int i;
+    
+    concat (s3, s1, 5, s2, 6);
+    
+    for ( i = 0; i < 11; i++ )
+        printf("%c", s3[i]);
+    printf("\n");
+    
+
+
+    
+    return 0;
+}
